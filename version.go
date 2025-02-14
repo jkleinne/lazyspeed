8244b6a719f1855578909f63d35587960b091ef8
jkleinne <jkq@jkleinne.com>
2025-02-14 02:59:15 -0500

    fix typo

--- version.go
+++ version.go
@@ -2,13 +2,11 @@ package main
 
 import "fmt"
 
-// Version information
 const (
 	Version   = "0.1.2"
-	BuildDate = "2024-02-14" // This should ideally be set during build time
+	BuildDate = "2025-02-14" // TODO: This should ideally be set during build time
 )
 
-// GetVersionInfo returns a formatted string containing version and build date
 func GetVersionInfo() string {
 	return fmt.Sprintf("lazyspeed version %s (built: %s)", Version, BuildDate)
 }
