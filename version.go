1d2b7d323f56a9e676b51e0556d3246cbde0101e
jkleinne <jkq@jkleinne.com>
2025-08-15 22:56:22 -0400

    refactor: use embedded build info instead of ldflags for versioning

--- version.go
+++ version.go
@@ -1,26 +1,52 @@
 package main
 
-import "fmt"
-
-var (
-	// Version holds the current version of the application. It can be overridden at
-	// build time using ldflags.
-	Version = "dev"
-	// BuildDate stores the date the binary was built. It can be set at build time
-	// using ldflags.
-	BuildDate string
+import (
+	"fmt"
+	"runtime/debug"
+	"time"
 )
 
-// GetVersionInfo returns a formatted version string. If BuildDate is empty, the
-// build date is omitted. If Version is empty, it defaults to "dev".
+// Returns a formatted version string using embedded build info
 func GetVersionInfo() string {
-	v := Version
-	if v == "" {
-		v = "dev"
+	version := "dev"
+	buildDate := ""
+	commitHash := ""
+
+	// Read embedded build information
+	if info, ok := debug.ReadBuildInfo(); ok {
+		// 1. Check for the main module version
+		if info.Main.Version != "" && info.Main.Version != "(devel)" {
+			version = info.Main.Version
+		}
+
+		// 2. Extract VCS information (used when building locally with 'go build' in a git repo)
+		for _, setting := range info.Settings {
+			switch setting.Key {
+			case "vcs.time":
+				t, err := time.Parse(time.RFC3339, setting.Value)
+				if err == nil {
+					buildDate = t.Format("2006-01-02")
+				}
+			case "vcs.revision":
+				// Use short commit hash (first 7 characters)
+				if len(setting.Value) > 7 {
+					commitHash = setting.Value[:7]
+				} else {
+					commitHash = setting.Value
+				}
+			}
+		}
+
+		// If we are in dev mode but found a commit hash, use it for clarity
+		if version == "dev" && commitHash != "" {
+			version = fmt.Sprintf("dev (%s)", commitHash)
+		}
 	}
 
-	if BuildDate == "" {
-		return fmt.Sprintf("lazyspeed version %s", v)
+	infoStr := fmt.Sprintf("lazyspeed version %s", version)
+
+	if buildDate != "" {
+		infoStr += fmt.Sprintf(" (built: %s)", buildDate)
 	}
-	return fmt.Sprintf("lazyspeed version %s (built: %s)", v, BuildDate)
+	return infoStr
 }
